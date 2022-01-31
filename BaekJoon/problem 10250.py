ffbc00e03c2bb2bb4dc32f9b80ce4ac9ac95a880
Jae-Soon Kwon <91954574+Jae-soon@users.noreply.github.com>
2022-01-31 16:35:28 +0900

    add baekjoon

---
+++ BaekJoon/problem 10250.py
@@ -0,0 +1,12 @@
+T = int(input())
+
+for _ in range(T):
+    H, W, guest = list(map(int, input().split()))
+    YY = guest % H
+    XX = (guest // H) + 1
+    
+    if YY == 0:
+        XX = XX - 1
+        YY = H
+    
+    print(YY * 100 + XX)
