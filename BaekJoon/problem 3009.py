1d85e2540ddda1930136a4d1611f5c0bfc75c902
Jae-Soon Kwon <rnjswotns12@naver.com>
2022-02-21 19:58:31 +0900

    add baekjoon

---
+++ BaekJoon/problem 3009.py
@@ -0,0 +1,14 @@
+x = list()
+y = list()
+
+for _ in range(3):
+    x1, y1 = map(int, input().split())
+    x.append(x1)
+    y.append(y1)
+
+for i in range(3):
+    if x.count(x[i]) == 1:
+        x4 = x[i]
+    if y.count(y[i]) == 1:
+        y4 = y[i]
+print(x4, y4)
