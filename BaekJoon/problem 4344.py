0af9e93147ae18f085a1fa61699cf52b7d128980
Jae-Soon Kwon <rnjswotns12@naver.com>
2022-03-23 02:50:14 +0900

    solve

---
+++ BaekJoon/problem 4344.py
@@ -0,0 +1,12 @@
+C = int(input())
+
+for _ in range(C):
+    score = list(map(int, input().split()))
+    avg = sum(score[1:]) / score[0]
+    cnt = 0
+
+    for personal_score in score[1:]:
+        if personal_score > avg:
+            cnt += 1
+    rate = cnt / score[0] * 100
+    print(f'{rate:.3f}%')
