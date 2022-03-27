fee2cbc9cfbe5064241b785a1a4b1912ea03888d
Jae-Soon Kwon <rnjswotns12@naver.com>
2022-03-28 03:18:36 +0900

    solve

--- BaekJoon/problem 2751.py
+++ BaekJoon/problem 2751.py
@@ -1,13 +1,12 @@
 import sys
 
 N = int(sys.stdin.readline())
-num = [0] * 10001
+num = list()
 
 for _ in range(N):
     a = int(sys.stdin.readline())
-    num[a] = num[a] + 1
+    num.append(a)
 
-for i in range(10001):
-    if num[i] != 0:
-        for _ in range(num[i]):
-            print(i)
+num.sort()
+for i in num:
+    print(i)
