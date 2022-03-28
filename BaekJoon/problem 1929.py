7fe91ccaabde03444c5867dbbcb52324c8156cb5
Jae-Soon Kwon <rnjswotns12@naver.com>
2022-03-29 02:51:58 +0900

    solve

--- BaekJoon/problem 1929.py
+++ BaekJoon/problem 1929.py
@@ -1,17 +1,12 @@
 M, N = map(int, input().split())
 arr=[]
+div_num = []
 
-for i in range(M,N+1):
-    if i==1:
-        pass
-    elif i==2:
-        arr.append(i)
+for i in range(M, N+1):
+    if i == 1:
+        continue
+    for j in range(2, int(i ** 0.5) + 1):
+        if i % j == 0:
+            break
     else:
-        for j in range(2, i):
-            if i%j==0:
-                break
-            elif j==i-1:
-                arr.append(i)
-
-for i in arr:
-    print(i)  
+        print(i)
