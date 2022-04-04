be73930b0f9e9b13b637c963fcf0671a71f51fe3
Jae-Soon Kwon <rnjswotns12@naver.com>
2022-04-04 17:49:39 +0900

    solve

--- BaekJoon/problem 11650.py
+++ BaekJoon/problem 11650.py
@@ -7,8 +7,9 @@ num_dic = dict()
 li = list()
 for _ in range(int(N)):
     li.append(list(map(int, inf.readline().split())))
-li.sort(key=lambda x: (x[0], x[1]))
+li.sort(key=lambda x: (x[1], x[0]))
 
+print(li)
 for i in li:
     print(i[0], i[1])
 
