370d7747919c5a98413a3955b0ce58513a583a41
Jae-Soon Kwon <rnjswotns12@naver.com>
2022-04-04 17:26:51 +0900

    solve

---
+++ BaekJoon/problem 11650.py
@@ -0,0 +1,15 @@
+import sys
+
+inf = sys.stdin
+N = inf.readline()
+
+num_dic = dict()
+li = list()
+for _ in range(int(N)):
+    li.append(list(map(int, input().split())))
+li.sort(key=lambda x: (x[0], x[1]))
+
+for i in li:
+    print(i[0], i[1])
+
+
