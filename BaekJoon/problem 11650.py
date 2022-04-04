2ed5a65daf0ab4e4aff8011f13f24cbe48211494
Jae-Soon Kwon <rnjswotns12@naver.com>
2022-04-04 17:27:43 +0900

    solve

--- BaekJoon/problem 11650.py
+++ BaekJoon/problem 11650.py
@@ -6,7 +6,7 @@ N = inf.readline()
 num_dic = dict()
 li = list()
 for _ in range(int(N)):
-    li.append(list(map(int, input().split())))
+    li.append(list(map(int, inf.readline().split())))
 li.sort(key=lambda x: (x[0], x[1]))
 
 for i in li:
