48356b9f2a4142003e0ab40ffe5fd44c769fd67e
Jae-Soon Kwon <rnjswotns12@naver.com>
2022-04-06 19:15:43 +0900

    time over

--- BaekJoon/problem 1019.py
+++ BaekJoon/problem 1019.py
@@ -1,15 +1,11 @@
-# import sys
+import sys
 
-# N = int(sys.stdin.readline())
-# num_list = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+N = int(sys.stdin.readline())
+num_list = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
-# for i in range(1, N + 1):
-#     str_i = str(i)
-#     for j in str_i:
-#         num_list[int(j)] += 1
+for i in range(1, N + 1):
+    str_i = str(i)
+    for j in str_i:
+        num_list[int(j)] += 1
         
-# print(num_list)
-
-str_i = str(10)
-for i in str_i:
-    print(i)
+print(num_list)
