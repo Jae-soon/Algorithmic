a7de8f60c4946749a72a1dfa135f7e2aafe5fd65
Jae-Soon Kwon <rnjswotns12@naver.com>
2022-04-06 21:49:26 +0900

    solve

--- BaekJoon/problem 1019.py
+++ BaekJoon/problem 1019.py
@@ -1,11 +1,27 @@
+# time over
+
 import sys
 
-N = int(sys.stdin.readline())
-num_list = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+n = int(sys.stdin.readline())
+num_list = [0] * 10
+point = 1
 
-for i in range(1, N + 1):
-    str_i = str(i)
-    for j in str_i:
-        num_list[int(j)] += 1
-        
-print(num_list)
+while n != 0:
+    while n % 10 != 9:
+        for i in str(n):
+            num_list[int(i)] += point
+        n -= 1
+    if n < 10:
+        for i in range(n+1):
+            num_list[i] += point
+        num_list[0] -= point
+        break
+    else:
+        for i in range(10):
+            num_list[i] += (n // 10 + 1) * point
+    num_list[0] -= point
+    point *= 10
+    n //= 10
+
+for i in num_list:
+    print(i, end=" ")
