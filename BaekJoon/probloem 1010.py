804a820029deae724f595d175806122d8b975ae1
Jae-Soon Kwon <rnjswotns12@naver.com>
2022-04-07 22:34:26 +0900

    solve

---
+++ BaekJoon/probloem 1010.py
@@ -0,0 +1,14 @@
+import sys
+
+def factorial(n):
+    num = 1
+    for i in range(1, n+1):
+        num *= i
+    return num
+
+T = sys.stdin.readline()
+
+for _ in range(int(T)):
+    n, m = map(int, sys.stdin.readline().split())
+    bridge = factorial(m) // (factorial(n) * factorial(m-n))
+    print(bridge)
