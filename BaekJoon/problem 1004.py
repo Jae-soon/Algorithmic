db4b9a7e15146c5c5156d637361929f7a13f5daa
Jae-Soon Kwon <rnjswotns12@naver.com>
2022-04-08 19:07:07 +0900

    solve

---
+++ BaekJoon/problem 1004.py
@@ -0,0 +1,23 @@
+import sys
+
+inf = sys.stdin
+T = inf.readline()
+
+for _ in range(int(T)):
+    x1, y1, x2, y2 = map(int, inf.readline().split())
+    n = int(inf.readline())
+    count = 0
+    for _ in range(n):
+        cx, cy, r = map(int, inf.readline().split())
+        dis1 = (x1-cx)**2 + (y1-cy)**2
+        dis2 = (x2-cx)**2 + (y2-cy)**2
+        cr = r**2
+
+        if cr > dis1 and cr > dis2:
+            pass
+        elif cr > dis1:
+            count += 1
+        elif cr > dis2:
+            count += 1
+    
+    print(count)
