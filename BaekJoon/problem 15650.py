fa09b26037acdf130467b9c11f2a67436866a836
Jae-Soon Kwon <rnjswotns12@naver.com>
2022-04-13 17:52:06 +0900

    solve

---
+++ BaekJoon/problem 15650.py
@@ -0,0 +1,16 @@
+n, m = map(int, input().split())
+
+s = []
+
+def f(num):
+    if len(s) == m:
+        print(' '.join(map(str, s)))
+        return
+    
+    for i in range(num, n+1):
+        if i not in s:
+            s.append(i)
+            f(i+1)
+            s.pop()
+
+f(1)
