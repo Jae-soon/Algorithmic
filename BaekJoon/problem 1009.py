720596e1bfe730a87a92e6f1ed7a1280295d4d85
Jae-Soon Kwon <rnjswotns12@naver.com>
2022-04-21 16:42:05 +0900

    solve

---
+++ BaekJoon/problem 1009.py
@@ -0,0 +1,31 @@
+import sys
+
+inf = sys.stdin
+t = inf.readline()
+
+for _ in range(int(t)):
+    a, b = map(int, input().split())
+    a = a % 10
+    
+    if a == 0:
+        print(10)
+    elif a == 1 or a == 5 or a == 6:
+        print(a)
+    elif a == 4 or a == 9:
+        b = b % 2
+        if b == 1:
+            print(a)
+        else:
+            print((a*a) % 10)
+    else:
+        b = b % 4
+        if b == 0:
+            print((a ** 4) % 10 % 10 % 10)
+        else:
+            print((a ** b) % 10 % 10 % 10)
+                            
+    #시간초과
+    # a, b = map(int, input().split())
+    
+    # data = list(str(a**b))
+    # print(data[-1])
