66e2f9d4527aded0f65e1dc3de6ec6488cc7d295
Jae-Soon Kwon <rnjswotns12@naver.com>
2022-07-06 21:43:19 +0900

    solve

--- BaekJoonJava/problem2439.java
+++ BaekJoonJava/problem2439.java
@@ -4,16 +4,15 @@ public class problem2439 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int n = sc.nextInt();
-
-        for (int i = 0; i <= n; i++) {
-            for (int j = 0; j <= n - i; ++j) {
-                System.out.print(" ");
-            }
-            for (int k = 0; k <= i; ++k) {
-                System.out.print("*");
-            }
-            System.out.println();
-        }
+		int N = sc.nextInt();
+		for (int i = 1; i <= N; i++) {
+			for (int j = 1; j <= N-i; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <= i; j++) {
+				System.out.print("*");
+			}
+			System.out.print("\n");
+		}
     }
 }
