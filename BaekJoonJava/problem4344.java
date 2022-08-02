f0400888f73930f91798d821d58f620cf8fcf22b
Jae-Soon Kwon <91954574+Jae-soon@users.noreply.github.com>
2022-08-03 00:54:24 +0900

    solve

---
+++ BaekJoonJava/problem4344.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class problem4344 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+        int[] score_arr;
+
+        for(int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int score_num = Integer.parseInt(st.nextToken());
+            double sum = 0;
+            score_arr = new int[score_num];
+            for(int j = 0; j < score_num; j++) {
+                int val = Integer.parseInt(st.nextToken());
+                sum += val;
+                score_arr[j] = val;
+            }
+
+            double avg = sum / score_num;
+            double count = 0;
+
+            for(int j = 0; j < score_num; j++) {
+                if(score_arr[j] > avg) {
+                    count++;
+                }
+            }
+
+            double result = count / score_num;
+            System.out.printf("%.3f%%\n", result * 100);
+        }
+
+    }
+}
---
+++ BaekJoonJava/problem8958.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class problem8958 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+
+        String arr[] = new String[n];
+
+        for(int i = 0; i < n; i++) {
+            arr[i] = br.readLine();
+        }
+
+        for(int i = 0; i < n; i++) {
+            int count = 0;
+            int sum = 0;
+
+            for (int j = 0; j < arr[i].length(); j++) {
+                if (arr[i].charAt(j) == 'O') {
+                    count++;
+                } else {
+                    count = 0;
+                }
+                sum += count;
+            }
+            sb.append(sum).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
