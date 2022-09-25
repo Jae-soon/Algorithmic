45d99ee951d16c53e4c3f31eb18652dc098d0a0d
Jae-Soon Kwon <91954574+Jae-soon@users.noreply.github.com>
2022-09-25 20:26:59 +0900

    solve

--- BaekJoonJava/problem2839.java
+++ BaekJoonJava/problem2839.java
@@ -15,15 +15,26 @@ public class problem2839 {
             }
             System.out.println(sum);
         } else {
-            if((n % 5) % 3 == 0) {
-                for(int i = 0; i < n / 5; i++) {
+//            if((n % 5) % 3 == 0) {
+            if(n % 5 == 1) {
+                for(int i = 0; i < (n / 5) - 1; i++) {
                     sum += 1;
                 }
 
-                for(int i = 0; i < (n % 5) / 3; i++) {
+                for(int i = 0; i < ((n % 5) / 3) + 2; i++) {
                     sum += 1;
                 }
                 System.out.println(sum);
+            } else if(n % 5 == 4) {
+                for(int i = 0; i < (n / 5) - 2; i++) {
+                    sum += 1;
+                }
+
+                for(int i = 0; i < (n % 5) / 3 + 3; i++) {
+                    sum += 1;
+                }
+                System.out.println(sum);
+
             } else if(n % 3 == 0) {
 				for(int i = 0; i < n / 3; i++) {
                     sum += 1;
