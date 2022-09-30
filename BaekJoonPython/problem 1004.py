09ecf3f67bb830bab2054de4cb5214b660167435
Jaesoooooooone <91954574+Jae-soon@users.noreply.github.com>
2022-09-30 23:09:40 +0900

    solve

--- BaekJoonPython/problem 1004.py
+++ BaekJoonPython/problem 1004.py
@@ -7,6 +7,7 @@ for _ in range(int(T)):
     x1, y1, x2, y2 = map(int, inf.readline().split())
     n = int(inf.readline())
     count = 0
+    st = 0
     for _ in range(n):
         cx, cy, r = map(int, inf.readline().split())
         dis1 = (x1-cx)**2 + (y1-cy)**2
