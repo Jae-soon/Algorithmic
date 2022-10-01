128d518fcb56c365855adf528dae1f4f128c98de
Jaesoooooooone <91954574+Jae-soon@users.noreply.github.com>
2022-10-02 00:02:07 +0900

    retry

---
+++ BaekJoonJava/problem11653.java
@@ -0,0 +1,9 @@
+public class problem11653 {
+    public static void main(String[] args) throw IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int n = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < )
+    }
+}
