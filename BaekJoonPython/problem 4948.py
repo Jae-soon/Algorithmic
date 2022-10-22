cedc40aedcc9472c1b8d43cc7d2d00f334b3038c
Jae-Soon Kwon <91954574+Jae-soon@users.noreply.github.com>
2022-10-22 12:36:21 +0900

    solve

--- BaekJoonPython/problem 4948.py
+++ BaekJoonPython/problem 4948.py
@@ -10,13 +10,13 @@ def prime(n):
     return True
 
 problem_list = list(range(2, 123456 * 2))
-memo = list()
+memo = list
 
 for i in problem_list:
     if prime(i):
         memo.append(i)
 
-n = sys.stdin.readline();
+n = int(sys.stdin.readline());
 
 while True:
     count = 0
@@ -28,4 +28,4 @@ while True:
             count += 1
         
     print(count)
-    n = sys.stdin.readline()
+    n = int(sys.stdin.readline());
