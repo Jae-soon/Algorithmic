d490c1cdf39e5acc61daf1ec32e5011eacc5127e
Jae-Soon Kwon <91954574+Jae-soon@users.noreply.github.com>
2022-11-01 22:52:42 +0900

    solve

--- Programmers/할인행사.java
+++ Programmers/할인행사.java
@@ -3,36 +3,71 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class 할인행사 {
-    public static void main(String[] args) {
-        String[] want = new String[]{"banana", "apple", "rice", "pork", "pot"};
-        int[] number = new int[]{3, 2, 2, 2, 1};
-        String[] discount = new String[]{"chicken", "apple", "apple", "banana", "rice", "apple", "pork", "banana", "pork", "rice", "pot", "banana", "apple", "banana"};
-    }
 
     public int solution(String[] want, int[] number, String[] discount) {
         int answer = 0;
 
-        for (int i = 0; i <= discount.length - 10; i++) {
-            String[] salesDay = Arrays.copyOfRange(discount, i, i+10);
-            int same_product = 0;
+        Map<String, Integer> wantMap = new HashMap<>();
 
-            for(int j = 0; j < want.length; j++) {
-                int count = 0;
-                for(String sale_product : salesDay) {
-                    if(want[j].equals(sale_product)) {
-                        count += 1;
-                    }
-                }
-                if(count == number[j]) {
-                    same_product += 1;
-                }
-            }
+        for(int i = 0; i < want.length; i++) {
+            String s = want[i];
+            int count = number[i];
 
-            if(same_product == 5) {
-                answer += 1;
+            wantMap.put(s, count);
+        }
+
+        Map<String, Integer> discountMap = new HashMap<>();
+        for (int i = 0; i < 10; i++) {
+            String addItem = discount[i];
+            discountMap.put(addItem, discountMap.getOrDefault(addItem, 0) + 1);
+        }
+
+        if(isEqual(wantMap, discountMap)) {
+            answer++;
+        }
+
+        for(int i = 1; i <= discount.length - 10; i++) {
+            String deleteItem = discount[i - 1];
+            discountMap.put(deleteItem, discountMap.get(deleteItem) - 1);
+
+            String addItem = discount[i - 1 + 10];
+            discountMap.put(addItem, discountMap.getOrDefault(addItem, 0) + 1);
+
+            if(isEqual(wantMap, discountMap)) {
+                answer++;
             }
         }
 
+//        for (int i = 0; i <= discount.length - 10; i++) {
+//            String[] salesDay = Arrays.copyOfRange(discount, i, i+10);
+//            int same_product = 0;
+//
+//            for(int j = 0; j < want.length; j++) {
+//                int count = 0;
+//                for(String sale_product : salesDay) {
+//                    if(want[j].equals(sale_product)) {
+//                        count += 1;
+//                    }
+//                }
+//                if(count == number[j]) {
+//                    same_product += 1;
+//                }
+//            }
+//it
+//            if(same_product == 5) {
+//                answer += 1;
+//            }
+//        }
+
         return answer;
+    }
+
+    public boolean isEqual(Map<String, Integer> wantMap, Map<String, Integer> discountMap) {
+        for(String key : wantMap.keySet()) {
+            if(!discountMap.containsKey(key) || wantMap.get(key) != discountMap.get(key)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
