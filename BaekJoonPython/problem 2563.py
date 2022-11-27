3a80ce05176e8a5015cbc2c145ff4ccf153021f9
Jaesoooooooone <91954574+Jae-soon@users.noreply.github.com>
2022-11-27 17:23:20 +0900

    solve

--- BaekJoonPython/problem 2563.py
+++ BaekJoonPython/problem 2563.py
@@ -6,8 +6,8 @@ paper = [[0 for _ in range(101)] for _ in range(101)]
 
 for _ in range(n):
     x, y = map(int, input().split())
-    for i in range(x, x+10):
-        for j in range(y, y+10):
+    for i in range(x, x + 10):
+        for j in range(y, y + 10):
             paper[i][j] = 1
 
 answer = 0
