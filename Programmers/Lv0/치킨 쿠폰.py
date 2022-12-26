c58ae0c3817d356ce69fa1f6d9b4a017319e5cd6
Jae-Soon Kwon <91954574+Jae-soon@users.noreply.github.com>
2022-12-26 22:34:40 +0900

    .

--- Programmers/Lv0/치킨 쿠폰.py
+++ Programmers/Lv0/치킨 쿠폰.py
@@ -1,6 +1,13 @@
 def solution(chicken):
     answer = -1
-    coupon = chicken // 10 
-    service = coupon // 10
-    rest = 
+    plus_chicken = 0
+    coupon_chicken = chicken // 10
+    rest_coupon = chicken % 10
+    coupon_chicken2 = coupon_chicken // 10
+    rest_coupon2 = coupon_chicken % 10
+    if rest_coupon + rest_coupon2 >= 10:
+        plus_chicken += ((rest_coupon + rest_coupon2) // 10)
+    
+    answer = plus_chicken + coupon_chicken + coupon_chicken2
+    
     return answer
