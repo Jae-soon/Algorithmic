0512722193ba4accccbebd6bfff1568d5e5a600a
Jaesoooooooone <91954574+Jae-soon@users.noreply.github.com>
2023-07-05 22:25:27 +0900

    solve

--- Programmers/Lv1/햄버거 만들기.py
+++ Programmers/Lv1/햄버거 만들기.py
@@ -1,14 +1,25 @@
+from collections import deque
+
 def solution(ingredient):
     answer = 0
+    idx = 0
+    li = []
     
-    s = ''.join(list(map(str, ingredient)))
+    for i in ingredient:
+        li.append(i)
+        
+        if li[-4:] == [1, 2, 3, 1]:
+            for _ in range(4):
+                li.pop()
+            answer += 1
+        
+#     s = ''.join(list(map(str, ingredient)))
     
-    while '1231' in s:
-        print(s)
-        for i in range(len(s)-4):
-            if '1231' == s[i:i+4]:
-                s = s[:i] + s[i+4:]
-                break
-    
-    
+#     while '1231' in s:
+#         answer += 1
+#         for i in range(len(s)-3):
+#             if '1231' == s[i:i+4]:
+#                 s = s[:i] + s[i+4:]
+#                 break
+
     return answer
