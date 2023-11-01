1ce4bb9dd24e72c985c525293d0faf653da51286
Jaesoooooooone <91954574+Jae-soon@users.noreply.github.com>
2023-11-01 21:10:49 +0900

    solve

--- Programmers/Hash/베스트앨범.py
+++ Programmers/Hash/베스트앨범.py
@@ -1,39 +1,33 @@
 def solution(genres, plays):
     answer = []
-    play_dict = {}
-    count_di = {}
-    genre_di = {}
-    sort_genres = sorted(genres)
-
-    for i in sort_genres:
-        genre_di[i] = []
-        count_di[i] = 0
     
-    for i, g in enumerate(genres):
-        count_di[g] += plays[i]
-        genre_di[g].append([i, plays[i]])
-
-    for i 
-
-    max_count_genre = max(count_di, key=count_di.get)
-
-    for k, v in genre_di.items():
-        count_li = []
-        if k == max_count_genre:
-            
-            answer.append(genre_di[k])
-
-    print(count_di)
-    print(genre_di)
-
-
-    # 값 채워넣기
-    for i, g in enumerate(genres):
-        key = g + str(i)
-        play_dict[key] = plays[i]
+    genre_play_time = {}
+    play_time = {}
+    play_num = {}
+    genre_count = {}
     
-    # for k, v in play_dict.items():
-    #     if k[0, -1] in sort_genres:
-            
-
+    for idx, genre in enumerate(genres):
+        genre_play_time[genre] = 0
+        genre_count[genre] = 0
+        play_time[genre + "/" + str(idx)] = 0
+    
+    for idx, genre in enumerate(genres):
+        genre_play_time[genre] += plays[idx]
+        play_time[genre + "/" + str(idx)] += plays[idx]
+        
+    genre_play_time = sorted(genre_play_time.items(), key = lambda item:item[1], reverse=True)
+    play_time = sorted(play_time.items(), key= lambda item:item[1], reverse=True)
+    sort_play_time = []
+    
+    for i in play_time:
+        if genre_count[i[0].split("/")[0]] >= 2:
+            continue
+        sort_play_time.append(i)
+        genre_count[i[0].split("/")[0]] += 1
+    
+    for i in genre_play_time:
+        for j in sort_play_time:
+            if i[0] in j[0].split("/"):
+                answer.append(int(j[0].split("/")[1]))
+    
     return answer
--- Programmers/Stack, Queue/다리를 지나는 트럭.py
+++ Programmers/Stack, Queue/다리를 지나는 트럭.py
@@ -1,29 +1,15 @@
-from collections import deque
-
 def solution(bridge_length, weight, truck_weights):
-    time = 0
-    wait = deque()
-    bridge = deque()
-    empty = deque([0] * len(bridge_length))
-    t_pass = []
-    for i in truck_weights:
-        wait.append(i)
-
-    while len(t_pass) != len(truck_weights):
-        if not wait:
-            t_pass.append(bridge.popleft())
-            time += 1
-            continue
-        truck = wait.popleft() # 트럭을 넣고
-        bridge.append(truck) # 다리에 트럭한대 입장
-        time += 1 # 시간은 증가
-
-        if sum(bridge) > weight or len(bridge) > bridge_length: # 만약 트럭 전체무게가 더 무겁다면
-            wait.appendleft(truck) # 들어온 트럭은 다시 빠지고
-            t_pass.append(bridge.popleft()) # 원래있던 트럭은 지나간다
-
+    answer = 0
+    on_bridge = [0] * bridge_length
     
-    
+    while len(on_bridge):
+        answer += 1
+        on_bridge.pop(0)
+        
+        if truck_weights:
+            if sum(on_bridge) + truck_weights[0] <= weight:
+                on_bridge.append(truck_weights.pop(0))
+            else:
+                on_bridge.append(0)
 
-    
-    return time
+    return answer
