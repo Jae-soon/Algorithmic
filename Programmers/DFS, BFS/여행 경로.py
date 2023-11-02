46591cf6c93ca71abf25dd5f729228108c038f30
Jaesoooooooone <91954574+Jae-soon@users.noreply.github.com>
2023-11-02 21:09:24 +0900

    solve2

--- Programmers/DFS, BFS/여행 경로.py
+++ Programmers/DFS, BFS/여행 경로.py
@@ -1,56 +1,18 @@
-from collections import deque, defaultdict
-
-answer = []
-graph = defaultdict(list)
-
-def dfs(s):
-    # print(graph[s])
-    while graph[s]:
-        dfs(graph[s].pop(0))
-    print(s)
-    if not graph[s]:
-        answer.append(s)
-        # print(graph)
-        # print(answer)
-        return
-
 def solution(tickets):
-
-    for start, end in tickets:
-        graph[start].append(end)
+    answer = []
+    visited = [False] * len(tickets)
     
-    print(graph)
-
-    for a, b in graph.items():
-        graph[a].sort()
-
-    print(graph)
-
-    dfs("ICN")
-
-    return answer[::-1]
-
-# def dfs(i, tickets, search):    
-#     answer = []
-#     start = "ICN"
-#     dq = deque()
-
-#     [start, end] = dq.popleft()
-#     if False not in search:
-#         return answer
-
-#     if tickets[i][0] == end and not search:
-#         answer.append(start)
-#         dq.append(tickets[i])
-#         search[i] = True
-    
-#     dfs(i + 1, tickets, search)
-
-
-# def solution(tickets):
-#     answer = []
-    
-#     search = [False] * len(tickets)
-
-#     dfs(0, tickets, search)
-#     return answer
+    def dfs(start, path):
+        if len(tickets) + 1 == len(path):
+            answer.append(path)
+            return
+        
+        for i, ticket in enumerate(tickets):
+            if ticket[0] == start and visited[i] == False:
+                visited[i] = True
+                dfs(ticket[1], path + [ticket[1]])
+                visited[i] = False
+                
+    dfs("ICN", ["ICN"])
+    answer.sort()
+    return answer[0]
