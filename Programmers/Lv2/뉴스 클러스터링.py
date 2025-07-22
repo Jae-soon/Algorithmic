2f74854efe5c4e12642eaee6923c0e86dd66337e
Jaesoooooooone <91954574+Jae-soon@users.noreply.github.com>
2025-07-22 23:03:07 +0900

    Update 뉴스 클러스터링.py
    solve

--- Programmers/Lv2/뉴스 클러스터링.py
+++ Programmers/Lv2/뉴스 클러스터링.py
@@ -2,34 +2,48 @@ import math
 
 def solution(str1, str2):
     answer = 0
-    str1_li = []
-    str2_li = []
-    same_list = []
-    diff_list = []
-
-    for i in range(0, len(str1) - 1):
-        if str1[i:i+2].upper().isalpha():
-            str1_li.append(str1[i:i+2].upper())
     
-    for i in range(0, len(str1) - 1):
-        if str2[i:i+2].upper().isalpha():
-            str2_li.append(str2[i:i+2].upper())
-
-    for i in str1_li:
-        if i in str2_li:
-            same_list.append(i)
-        else:
-            diff_list.append(i)
-    
-    for i in str2_li:
-        if i not in str1_li:
-            diff_list.append(i)
-    
-    diff_list = same_list + diff_list
-
-    if len(same_list) == 0 or len(diff_list) == 0:
-        answer = 1
-    else:
-        answer = math.floor((len(same_list) / (len(diff_list) + len(same_list))) * 65536)
+    str1_list = []
+    str2_list = []
+    for i in range(len(str1) - 1):
+        str1_list.append(str1[i:i+2].lower())
         
-    return answer
+    for i in range(len(str2) - 1):
+        str2_list.append(str2[i:i+2].lower())
+        
+    if str1_list == [] or str2_list == []:
+        return 65536
+    
+    str1_filtered = [x for x in str1_list if x.isalpha()]
+    str2_filtered = [x for x in str2_list if x.isalpha()]
+    
+    str1_dict = {}
+    str2_dict = {}
+    
+    for i in str1_filtered:
+        if i in str1_dict:
+            str1_dict[i] += 1
+        else:
+            str1_dict[i] = 1
+    
+    for i in str2_filtered:
+        if i in str2_dict:
+            str2_dict[i] += 1
+        else:
+            str2_dict[i] = 1
+
+    intersection = set(str1_dict.keys()).intersection(set(str2_dict.keys()))
+    union = set(str1_dict.keys()).union(set(str2_dict.keys()))
+    
+    intersection_count = 0
+    union_count = 0
+    
+    for i in intersection:
+        intersection_count += min(str1_dict.get(i, 0), str2_dict.get(i, 0))
+
+    for i in union:
+        union_count += max(str1_dict.get(i, 0), str2_dict.get(i, 0))
+        
+    answer = intersection_count / union_count if union_count != 0 else 1
+         
+    return math.floor(answer * 65536)
